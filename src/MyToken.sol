bb6aefebf3193b5486230948b89d694b992c45c4
Yashmevada2807 <yashmevada07@gmail.com>
2026-05-24 11:59:44 +0530

    initialize git

---
+++ src/MyToken.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20{
+
+    constructor() ERC20("Indian Rupee Coin","INRC"){
+        _mint(msg.sender, 10000 ether);
+    }
+}
