e7fe346bfcd2e6efa47e6068b61da31ec2c75d49
Yashmevada2807 <yashmevada07@gmail.com>
2026-05-25 17:20:58 +0530

    set format

--- src/BuyTokens.sol
+++ src/BuyTokens.sol
@@ -5,8 +5,7 @@ pragma solidity ^0.8.13;
 import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import {ReentrancyGuard} from "openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
 
-contract BuyTokens is ReentrancyGuard{
-
+contract BuyTokens is ReentrancyGuard {
     error UnAuthorized();
     error NotEnoughTokens();
     error InsufficientBalance();
@@ -18,17 +17,18 @@ contract BuyTokens is ReentrancyGuard{
     IERC20 public token;
     address public immutable owner;
     uint256 public perTokenPrice = 0.0001 ether;
-    constructor(address tokenAddress){
+
+    constructor(address tokenAddress) {
         token = IERC20(tokenAddress);
         owner = msg.sender;
-    } 
+    }
 
-    modifier onlyOwner {
-        if(msg.sender != owner) revert UnAuthorized();
+    modifier onlyOwner() {
+        if (msg.sender != owner) revert UnAuthorized();
         _;
     }
 
-    struct Buyer{
+    struct Buyer {
         address tokenHolder;
         uint256 tokensHold;
         uint256 time;
@@ -38,35 +38,28 @@ contract BuyTokens is ReentrancyGuard{
 
     event BuyerDetail(address holderName, uint256 amount, uint256 time);
 
-    function buyToken(uint256 _amountOfToken) external payable nonReentrant{
-        if(_amountOfToken == 0) revert InvalidAmount();
-        
+    function buyToken(uint256 _amountOfToken) external payable nonReentrant {
+        if (_amountOfToken == 0) revert InvalidAmount();
+
         uint256 finalPay = _amountOfToken * perTokenPrice;
 
-        if(token.balanceOf(address(this)) < _amountOfToken ) revert NotEnoughTokens();
+        if (token.balanceOf(address(this)) < _amountOfToken) revert NotEnoughTokens();
 
-        if(msg.value !== finalPay) revert InsufficientBalance(); 
-        buyers[msg.sender] = Buyer({
-            tokenHolder: msg.sender;
-            tokensHold: _amountOfToken;
-            time: block.timestamp;
-        });
+        if (msg.value != finalPay) revert InsufficientBalance();
+        buyers[msg.sender] = Buyer({tokenHolder: msg.sender, tokensHold: _amountOfToken, time: block.timestamp});
 
         bool success = token.transfer(msg.sender, _amountOfToken);
-        if(!success) revert TransactionFailed();
+        if (!success) revert TransactionFailed();
 
         emit BuyerDetail(msg.sender, _amountOfToken, block.timestamp);
     }
 
-
-    function withdrawAmount() external onlyOwner nonReentrant{
+    function withdrawAmount() external onlyOwner nonReentrant {
         uint256 balance = address(this).balance;
-        if(balance == 0) revert NoAmountToWithdraw();
+        if (balance == 0) revert NoAmountToWithdraw();
 
-        (bool success, ) = payable(owner).call{value: balance}("");
+        (bool success,) = payable(owner).call{value: balance}("");
 
-        if(!success) revert TransactionFailed();
+        if (!success) revert TransactionFailed();
     }
-
-
 }
